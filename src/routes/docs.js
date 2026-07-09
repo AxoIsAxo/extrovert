2dcc179d75128b232b5f40a7202aca2069d3e597
axoisaxo <axoisaxo@pm.me>
2026-07-09 14:20:22 +0200

    Nuclear Swagger UI theme: !important on all overrides, pin CDN version, kill :visited purple

--- src/routes/docs.js
+++ src/routes/docs.js
@@ -12,8 +12,7 @@ router.get('/openapi.json', (req, res) => {
 
 // Serve Swagger UI
 router.get('/docs', (req, res) => {
-  // Helmet's CSP blocks CDN. Remove it, then set our own.
-  res.removeHeader('Content-Security-Policy');
+  // Override CSP for this route to allow Swagger UI CDN assets
   res.set('Content-Security-Policy',
     "default-src 'self'; " +
     "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.jsdelivr.net; " +
